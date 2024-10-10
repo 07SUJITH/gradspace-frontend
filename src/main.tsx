f6bd60b8a4b8deef650d30d7ce5600baba161859
07SUJITH <tssujith2002@gmail.com>
2024-10-10 15:46:30 +0530

    chore: Add Toaster component for displaying notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import './index.css';
 
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.tsx';
@@ -8,5 +9,14 @@ import App from './App.tsx';
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
+    <Toaster
+      toastOptions={{
+        style: {
+          padding: '7px',
+          border: '2px solid #E1C6E6',
+          zIndex: 9999,
+        },
+      }}
+    />
   </BrowserRouter>,
 );
