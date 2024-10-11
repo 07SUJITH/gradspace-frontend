602ad5c00963f9303ce074b51b6f236f1876ab13
07SUJITH <tssujith2002@gmail.com>
2024-10-11 17:55:15 +0530

    feat: Add Dashboard and DevInfo pages route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
+import Dashboard from './pages/Dashboard';
+import DevInfo from './pages/DevInfo';
 import Home from './pages/Home'; // Your main home component
 import Login from './pages/Login'; // Your login component
 import Signup from './pages/Signup'; // Your signup component
@@ -10,6 +12,8 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="devinfo" element={<DevInfo />} />
     </Routes>
   );
 };
